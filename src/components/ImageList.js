e763621aecb8e0871f745e15dc3b99a44887d62f
Le Thanh <home@Les-iMac.local>
2020-09-18 17:31:52 +0700

    finish app

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,17 +1,12 @@
 import React from 'react';
+import ImageCard from './ImageCard';
 import './ImageList.css';
 
 const ImageList = ({ images }) => {
 	return (
 		<div className='image-list'>
 			{images.map((image) => {
-				return (
-					<img
-						key={image.id}
-						src={image.urls.regular}
-						alt={image.description}
-					/>
-				);
+				return <ImageCard key={image.id} image={image} />;
 			})}
 		</div>
 	);
